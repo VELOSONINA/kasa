f64a603f7a31b349d8b520f929713b06e872be38
Nirina VELOSONINA <vnirina081@gmail.com>
2023-07-13 04:03:58 +0200

    ajout du fichier Buttons.jsx et Button.scssdans Accomodities

--- src/Components/Accomodities/Buttons.jsx
+++ src/Components/Accomodities/Buttons.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+// import Host from '../../assets/images/Host.png';
 
-const Buttons = () => {
+const Buttons = ({ imageHost }) => {
   return (
-    <div className='buttons'>
-      <h1>Cozy loft the Canal Saint Martin</h1>
-      <h2>Paris, île-de-France</h2>
-      <button>Cozy</button>
-      <button>Canal</button>
-      <button>Paris 10</button>
-      <div className='stars'>
-        <h3>Alexandre Duma</h3>
-        <span class='material-symbols-outlined'>star</span>
-        <span class='material-symbols-outlined'>star</span>
-        <span class='material-symbols-outlined'>star</span>
-        <span class='material-symbols-outlined'>star</span>
-        <span class='material-symbols-outlined'>star</span>
+    <div className='titles'>
+      <div className='titles-content'>
+        <h1>Cozy loft the Canal Saint Martin</h1>
+        <h2>Paris, île-de-France</h2>
+        <div className='buttons-content'>
+          <button>Cozy</button>
+          <button>Canal</button>
+          <button>Paris 10</button>
+        </div>
+      </div>
+      <div className='icons-container'>
+        <div className='content-icons'>
+          <h3>
+            Alexandre
+            <span>Duma</span>
+          </h3>
+          {/* <img src={(imageHost = Host)} alt='' /> */}
+          <div className='circle-icon'></div>
+        </div>
+        <div className='stars'>
+          <span class='material-symbols-outlined'>star</span>
+          <span class='material-symbols-outlined'>star</span>
+          <span class='material-symbols-outlined'>star</span>
+          <span class='material-symbols-outlined'>star</span>
+          <span class='material-symbols-outlined'>star</span>
+        </div>
       </div>
     </div>
   );
--- src/Components/Accomodities/Carousel.jsx
+++ src/Components/Accomodities/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React, { useState } from 'react';
 import logements from '../Data/logements.json';
 
 const Carousel = () => {
