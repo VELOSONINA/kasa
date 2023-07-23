4429e967189f87fb9a670270192ebbec69cc08ee
Nirina VELOSONINA <vnirina081@gmail.com>
2023-07-23 02:20:36 +0200

    modification et mise en place des numérotations dans Carousel

--- src/Components/Accomodities/Carousel.jsx
+++ src/Components/Accomodities/Carousel.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import arrowLeft from '../../assets/images/arrow_left.png';
+import arrowRight from '../../assets/images/arrow_right.png';
 
 const Carousel = (props) => {
   const { logement } = props;
   const { pictures } = logement;
-  const imageCarousel = pictures.map((picture, index) => (
-    <img key={index} src={picture} alt='' />
-  ));
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % pictures.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + pictures.length) % pictures.length
+    );
+  };
 
   return (
     <div className='carousel'>
-      {imageCarousel}
-      <div className='accordion-content-container'></div>
+      <div className='slide-container'>
+        <img src={pictures[currentSlide]} alt='' />
+      </div>
+      <div className='slide-numeration'>
+        <p>
+          {currentSlide + 1} / {pictures.length}
+        </p>
+      </div>
+      <div className='arrow-container'>
+        <img src={arrowLeft} alt='' onClick={prevSlide} />
+        <img src={arrowRight} alt='' onClick={nextSlide} />
+      </div>
     </div>
   );
 };
