e967c2028b18034ca861be3eb3a5a7e09fed24fe
Nirina VELOSONINA <vnirina081@gmail.com>
2023-07-23 02:31:03 +0200

    modification dans Gallery

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Cards from './Cards';
-import dataLogements from '../../assets/Data/logements.json';
+import logements from '../../assets/Data/logements.json';
 
 const Gallery = () => {
   return (
     <div className='gallery'>
-      {dataLogements.map((logement) => (
+      {logements.map((logement) => (
         <Cards key={logement.id} logement={logement} />
       ))}
     </div>
