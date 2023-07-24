7edaeaa0638815694904b931d7209efee2fbae74
Nirina VELOSONINA <vnirina081@gmail.com>
2023-07-24 20:23:28 +0200

    ajout route Accomodities

--- src/Components/RoutesTree.jsx
+++ src/Components/RoutesTree.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../Pages/Home';
 import About from '../Pages/About';
-import Accomodities from './Accomodities/Accomodities';
+import Accomodities from '../Components/Accomodities/Accomodities';
 import ErrorPage from '../Pages/ErrorPage';
+import logements from '../assets/Data/logements.json';
 
 const RoutesTree = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
-      <Route path='/Accomodities/:id' element={<Accomodities />} />
+      <Route
+        path='/Accomodities/:id'
+        element={<Accomodities logements={logements} />}
+      />
       <Route path='*' element={<ErrorPage />} />
     </Routes>
   );
