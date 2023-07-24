6386750411bf208d63982209c855c4d883c92aea
Nirina VELOSONINA <vnirina081@gmail.com>
2023-07-24 20:21:57 +0200

    ajout de loading

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Cards from './Cards';
-import logements from '../../assets/Data/logements.json';
+import logementsData from '../../assets/Data/logements.json';
 
 const Gallery = () => {
+  const [loading, setLoading] = useState(true);
+  const [logements, setLogements] = useState([]);
+
+  useEffect(() => {
+    // fonction asynchrone pour charger les données
+    const fetchData = async () => {
+      try {
+        // délai pour charger les données
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        setLogements(logementsData);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+
+    // Appel de la fonction fetchData pour charger les données
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div className='spinner' />;
+  }
+
   return (
     <div className='gallery'>
       {logements.map((logement) => (
